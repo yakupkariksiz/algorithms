a4136ff974e181562ae1c39e57b7524b4a8ebd75
yakup.kariksiz <yakup.kariksiz@epias.com.tr>
2021-02-14 12:03:39 +0300

    chor: add new linkedlist challenges.

--- src/main/java/linkedlists/LinkedList.java
+++ src/main/java/linkedlists/LinkedList.java
@@ -1,24 +1,47 @@
 package linkedlists;
 
+import java.util.ArrayList;
+import java.util.List;
+
 class LinkedList {
 
     public static void main(String[] args) {
-        DoublyLinkedListNode head = new DoublyLinkedListNode(1);
-        DoublyLinkedListNode second = new DoublyLinkedListNode(2);
-        DoublyLinkedListNode third = new DoublyLinkedListNode(3);
-        DoublyLinkedListNode fourth = new DoublyLinkedListNode(4);
-        head.next = second;
-        second.prev = head;
-        second.next = third;
-        third.prev = second;
-        third.next = fourth;
-        fourth.prev = third;
-        DoublyLinkedListNode node = reverse(head);
+        SinglyLinkedListNode _01 = new SinglyLinkedListNode(4);
+        SinglyLinkedListNode _02 = new SinglyLinkedListNode(5);
+        SinglyLinkedListNode _03 = new SinglyLinkedListNode(3);
+        SinglyLinkedListNode _04 = new SinglyLinkedListNode(2);
+
+        _01.next = _02;
+        _02.next = _03;
+        _03.next = _04;
+
+        SinglyLinkedListNode _11 = new SinglyLinkedListNode(7);
+        SinglyLinkedListNode _12 = new SinglyLinkedListNode(8);
+        SinglyLinkedListNode _13 = new SinglyLinkedListNode(5);
+        SinglyLinkedListNode _14 = new SinglyLinkedListNode(1);
+        SinglyLinkedListNode _15 = new SinglyLinkedListNode(6);
+        SinglyLinkedListNode _16 = new SinglyLinkedListNode(44);
+
+        _11.next = _12;
+        _12.next = _13;
+        _13.next = _14;
+        _14.next = _15;
+        _15.next = _16;
+        _16.next = _04;
+
+        System.out.println(findMergeNode(_01, _11));
+
+        printLinkedList(_01);
+        printLinkedList(_11);
+    }
+
+    static void printLinkedList(SinglyLinkedListNode node) {
         while (node != null) {
-            System.out.print(node.data + " <-> ");
+            System.out.print(node.data + " -> ");
             node = node.next;
         }
         System.out.print(" null ");
+        System.out.println();
     }
 
     static SinglyLinkedListNode insertNodeAtPosition(SinglyLinkedListNode head, int data, int position) {
@@ -90,6 +113,43 @@ class LinkedList {
         }
         return newHead;
     }
+
+    static int findMergeNode(SinglyLinkedListNode head1, SinglyLinkedListNode head2) {
+
+        SinglyLinkedListNode current1 = head1;
+        SinglyLinkedListNode current2 = head2;
+
+        while (current1 != null) {
+            current2 = head2;
+            while (current2 != null) {
+                if (current1 == current2) {
+                    return current1.data;
+                }
+                current2 = current2.next;
+            }
+            current1 = current1.next;
+        }
+
+        return 0;
+    }
+
+    static boolean hasCycle(Node head) {
+        Node current = head;
+        List<Node> visits = new ArrayList<>();
+        while (current != null) {
+            if (visits.contains(current)) {
+                return true;
+            }
+            visits.add(current);
+            current = current.next;
+        }
+        return false;
+    }
+}
+
+class Node {
+    int data;
+    Node next;
 }
 
 class SinglyLinkedListNode {
